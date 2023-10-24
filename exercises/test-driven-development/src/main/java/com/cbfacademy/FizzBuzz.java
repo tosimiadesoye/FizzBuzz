57141ed18639bce4cd9241fa59eb68acfcde4658
Tosimi <tosimiadesoye111@gmail.com>
2023-10-24 20:40:07 +0100

    add test

---
+++ exercises/test-driven-development/src/main/java/com/cbfacademy/FizzBuzz.java
@@ -0,0 +1,11 @@
+package com.cbfacademy;
+
+public class FizzBuzz {
+        // Create a list that contains a nums from 1-15
+        // if it a multiple of 3 return Fizz
+        // if it a multiple of 5 return Buzz
+        // if it a multiple of 15 return FizzBuzz
+        public static String of(int num) {
+                return null;
+        }
+}
---
+++ exercises/test-driven-development/src/test/java/com/cbfacademy/FizzBuzzTests.java
@@ -0,0 +1,34 @@
+package com.cbfacademy;
+
+import org.junit.jupiter.api.DisplayName;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.stream.Stream;
+
+
+@DisplayName("The FizzBuzz class should")
+public class FizzBuzzTests {
+    @ParameterizedTest
+    @MethodSource("exerciseArguements")
+    @DisplayName("should return number, 'Fizz', 'Buzz'or'FizzBuzz'")
+
+    public void shouldReturnCorrectOutput(Integer value, String expected) {
+        final String actual = FizzBuzz.of(value);
+
+        assertThat(actual, is(expected));
+    }
+        public static Stream<Arguments> exercise1Arguments(){
+            return Stream.of(
+                    Arguments.of(1, "1"),
+                    Arguments.of(3, "Fizz"),
+                    Arguments.of(6, "Fizz"),
+                    Arguments.of(21, "Fizz")
+                    );
+        }
+}
