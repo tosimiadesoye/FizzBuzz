5fd3625c5cd4fdaff458fe5da6ab58a3550626e6
Tosimi <tosimiadesoye111@gmail.com>
2023-10-25 09:17:27 +0100

    add code for fizzbuzz

--- exercises/test-driven-development/src/main/java/com/cbfacademy/FizzBuzz.java
+++ exercises/test-driven-development/src/main/java/com/cbfacademy/FizzBuzz.java
@@ -1,16 +1,14 @@
 package com.cbfacademy;
 
 public class FizzBuzz {
-        // Create a list that contains a nums from 1-15
-        // if it a multiple of 3 return Fizz
-        // if it a multiple of 5 return Buzz
-        // if it a multiple of 15 return FizzBuzz
-        public static String of(int num) {
-                if(num % 3 == 0){
-                        return "Fizz";
-                } else if (num % 5 == 0){
-                        return "Buzz";
-                }
-                return String.valueOf(num);
+    public static String of(int num) {
+        if (num % 15 == 0) {
+            return "FizzBuzz";
+        } else if (num % 5 == 0) {
+            return "Buzz";
+        } else if (num % 3 == 0) {
+            return "Fizz";
         }
+        return String.valueOf(num);
+    }
 }
