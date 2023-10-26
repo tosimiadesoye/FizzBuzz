d038437c29e8ce1bb93981273cd4344e0acf24d6
Phoebe Olaniyan <imjustgorgeous@hotmail.co.uk>
2023-10-26 20:20:45 +0100

    Completed exercise

--- exercises/test-driven-development/src/test/java/com/cbfacademy/LeapYearTest.java
+++ exercises/test-driven-development/src/test/java/com/cbfacademy/LeapYearTest.java
@@ -23,9 +23,40 @@ public class LeapYearTest {
                 Arguments.of(800, true),
                 Arguments.of(2000, true),
                 Arguments.of(2008, true),
-                Arguments.of(1300, true)
+                Arguments.of(1300, false),
+                Arguments.of(1800,false),
+                Arguments.of(2001,false),
+                Arguments.of(2002,false),
+                Arguments.of(2003,false),
+                Arguments.of(2004,true),
+                Arguments.of(2005,false),
+                Arguments.of(2006,false),
+                Arguments.of(2007,false),
+                Arguments.of(2009,false),
+                Arguments.of(2010,false),
+                Arguments.of(2011,false)
+                
+               
+               
+
         );
     }
+
+//     1700 false
+// 1800 false
+// 1900 false
+// 2000 true
+// 2001 false
+// 2002 false
+// 2003 false
+// 2004 true
+// 2005 false
+// 2006 false
+// 2007 false
+// 2008 true
+// 2009 false
+// 2010 false
+// 2011 false
 
     @ParameterizedTest
     @MethodSource("exercise1Arguments")
